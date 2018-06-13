4d6d213309b0a611b18cb14a551e4666777759ff
Varsha S <37741718+varshasathya@users.noreply.github.com>
2018-06-13 13:39:28 +0530

    Update Day 0: Mean, Median, and Mode.py

--- Day 0: Mean, Median, and Mode.py
+++ Day 0: Mean, Median, and Mode.py
@@ -1,15 +1,17 @@
-"""Objective: 
-In this challenge, we practice calculating the mean, median, and mode. Check out the Tutorial tab for learning materials and an 
-instructional video!
 
-Sample Input:
+#Objective: 
+#In this challenge, we practice calculating the mean, median, and mode. Check out the Tutorial tab for learning materials and an 
+#instructional video!
+
+#Sample Input:
 10
 64630 11735 14216 99233 14470 4978 73429 38120 51135 67060
 
-Sample Output:
+#Sample Output:
 43900.6
 44627.5
-4978"""
+4978
+
 
 import numpy as np
 from scipy import stats
